c1a85bcad3714eeb7d92a8c33f26167447ed00a7
William Ng <84236898+williamng95@users.noreply.github.com>
2021-10-06 11:29:57 +0800

    upload palindrome

---
+++ JSAlgosDataStructures/palindrome.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+    let regEx=/[A-Za-z0-9]/gi
+    let forwardArr=str.toLowerCase().match(regEx)
+    let reverseArr=[]
+    for (let i=forwardArr.length;i>0;i--){
+      reverseArr.push(forwardArr[i-1])
+    }
+    return forwardArr.join()==reverseArr;
+  }
+  
+  
+  
+  console.log(palindrome("eye"));
